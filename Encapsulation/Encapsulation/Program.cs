afc5b46d8d43f0a2df33a8c2d2aaec9777acc280
AbbasovMahir22 <mahir.abbasov.2022@mail.ru>
2024-08-18 00:31:50 +0400

    Created

---
+++ Encapsulation/Encapsulation/Program.cs
@@ -0,0 +1,17 @@
+﻿using Encapsulation;
+
+User user = new User();
+Console.WriteLine(user.HasDigit(Console.ReadLine()));
+
+User user0 = new User();
+Console.WriteLine(user0.HasUpper(Console.ReadLine()));
+
+User user1 = new User();
+Console.WriteLine(user1.HasLower(Console.ReadLine()));
+
+User user2 = new()
+{
+    username = "nicatorucovbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb",
+    password = "nicat223"
+};
+Console.WriteLine($"{user2.username}  {user2.password}");
